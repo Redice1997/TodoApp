3ded2409e126250201a50b9d53b0681d52924acf
Redice1997 <roshin-vladimir@mail.ru>
2023-11-26 21:46:52 +0300

    Use Swagger

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -4,7 +4,19 @@ using TodoApp.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.MapGet("/items", async (TodoDb db) => 
     await db.Todos.ToListAsync());
