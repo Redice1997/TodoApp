ab21d4e55d6a88fe57ffe40e80eaa19773e2542b
Redice1997 <roshin-vladimir@mail.ru>
2023-11-26 20:16:14 +0300

    add CRUD

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -1,6 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+using TodoApp.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/items", async (TodoDb db) => 
+    await db.Todos.ToListAsync());
+
+app.MapGet("/items/complete", async (TodoDb db) =>
+    await db.Todos.Where(x => x.IsComplete).ToListAsync());
+
+app.MapGet("/items/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+app.MapPost("/items", async (Todo item, TodoDb db) =>
+{
+    db.Todos.Add(item);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/items/{item.Id}", item);
+});
+
+app.MapPut("/items/{id}", async (int id, Todo item, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = item.Name;
+    todo.IsComplete = item.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/items/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
 
 app.Run();
---
+++ TodoApp/Repository/TodoDb.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+namespace TodoApp.Repository
+{
+    public class TodoDb : DbContext
+    {
+        private readonly string connectionString;
+        public TodoDb(string connectionString) {
+            this.connectionString = connectionString;
+            Database.EnsureCreated(); 
+        }
+         
+        public DbSet<Todo> Todos => Set<Todo>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
